7bb3c9a42b9329a8e7929ad3cb67e5dcdbb4b1ac
11Giri <happygautam2016@gmail.com>
2023-01-03 15:50:28 -0500

    my git demo

---
+++ src/git/Test.java
@@ -0,0 +1,18 @@
+package git;
+
+public class Test {
+	public static void main(String[] args) throws InterruptedException {
+//		try {
+//	for(int i=0;i<=10;i++) {
+//		System.out.println(i);
+//		Thread.sleep(1000);
+//	}
+//	}catch(Exception e) {
+//		System.out.println(e);
+//	}
+		for(int i=0;i<=10;i++) {
+			System.out.println(i);
+			Thread.sleep(1000);
+}
+	}
+}
